55427bdbd0c1852279daf2e8d168dfc43c98c4a0
Nathan PROSPERT <nathpros@hotmail.fr>
2017-10-20 22:55:44 +0100

    Ajout level state + timer sleeping

--- Assets/Scripts/GameController.cs
+++ Assets/Scripts/GameController.cs
@@ -7,32 +7,55 @@ public class GameController : MonoBehaviour {
     public int randomNbActionMin;
     public int randomNbActionMax;
 
-    private LevelState levelState;
+    public int delaySleeping;
+
+    private LevelState _levelState;
 
 	// Use this for initialization
 	void Start () {
+        SetupGame();
 	}
 	
 	// Update is called once per frame
 	void Update () {
 
-        switch(levelState)
+        switch(_levelState) // BOUCLE DE JEU
         {
             case LevelState.running:
                 //DO SOMETHING
                 break;
             case LevelState.dreaming:
-                //DO SOMETHING
+                StartCoroutine(DelaySleeping());
                 break;
         }
 		
 	}
+    
+    // Le temps de sommeil
+    IEnumerator DelaySleeping()
+    {
+        yield return new WaitForSeconds(delaySleeping);
+        SetLevelState(LevelState.running);
+    }
 
     // Génère le nombre d'action donné au player
     public int GenerateActionToPlayer()
     {
         return Random.Range(randomNbActionMin, randomNbActionMax);
     }
+
+    // Change l'état du level 
+    public void SetLevelState(LevelState newState)
+    {
+        _levelState = newState;
+    }
+
+    // Setup les différents paramètre de la partie
+    public void SetupGame()
+    {
+        GenerateActionToPlayer();
+    }
+
 }
 
 public enum LevelState

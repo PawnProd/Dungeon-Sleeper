9eca8706c8c8901898ca3133929bcb7e3e9aec2e
adrien <adrienrochard@gmail.com>
2017-10-20 22:23:16 +0200

    menue : menu manager, button play/quit et background ( et avec le select all)

---
+++ Assets/Scripts/MenuManager.cs
@@ -0,0 +1,27 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+
+public class MenuManager : MonoBehaviour {
+
+    public void PlayGame()
+    {
+        SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex + 1);
+    }
+
+    public void Dormir()
+    {
+
+        #if UNITY_EDITOR
+            UnityEditor.EditorApplication.isPlaying = false;
+
+        #else
+		    Application.Quit() ;
+
+        #endif
+    }
+}
+
+   

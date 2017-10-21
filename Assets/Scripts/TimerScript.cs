8809166d2c5c827277eb70d1a1f96b2b56591165
adrien <adrienrochard@gmail.com>
2017-10-21 21:06:40 +0200

    menu échap ajout du coussin tuto termine

---
+++ Assets/Scripts/TimerScript.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+using System.Collections;
+using UnityEngine.UI;
+
+public class TimerScript : MonoBehaviour
+{
+    public static float timer;
+
+
+    // Use this before initialization
+    void Start()
+    {
+
+    }
+
+    void Update()
+    {
+        timer += Time.deltaTime;
+        GetComponent<Text>().text = string.Format("{0:0}:{1:00}", Mathf.Floor(timer / 60), timer % 60);
+
+    }
+
+}
